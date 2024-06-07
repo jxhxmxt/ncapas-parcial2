44dabbbdb001123ee5b24df2b72b4a0009a1cf46
Steve9452 <00380119@uca.edu.sv>
2024-06-07 17:13:22 -0600

    Add whoami route for debug only

--- src/main/java/com/uca/clinic/controllers/AuthController.java
+++ src/main/java/com/uca/clinic/controllers/AuthController.java
@@ -12,11 +12,10 @@ import jakarta.validation.Valid;
 import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.parameters.P;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -73,6 +72,13 @@ public class AuthController {
 
     }
 
+
+//    DEBUG ONLY
+    @GetMapping("/whoami")
+    public ResponseEntity<GeneralResponse> whoAmI(@AuthenticationPrincipal User userDetails){
+        return GeneralResponse.getResponse(HttpStatus.OK, "User logged in", userDetails.getEmail());
+
+    }
 
 
 
