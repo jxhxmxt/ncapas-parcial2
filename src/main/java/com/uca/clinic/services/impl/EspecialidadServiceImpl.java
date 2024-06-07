6f363565a16131621f6b47c8dd613cc996e9ae07
jmontecinos <jmontecinos@ofbox.com>
2024-06-07 13:58:13 -0600

    🚧 feat: Add Especialidad repository and service interfaces and implementations

---
+++ src/main/java/com/uca/clinic/services/impl/EspecialidadServiceImpl.java
@@ -0,0 +1,40 @@
+package com.uca.clinic.services.impl;
+
+import com.uca.clinic.domain.entities.Especialidad;
+import com.uca.clinic.repositories.EspecialidadRepository;
+import com.uca.clinic.services.EspecialidadService;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Component
+public class EspecialidadServiceImpl implements EspecialidadService {
+
+    private final EspecialidadRepository especialidadRepository;
+
+    public EspecialidadServiceImpl(EspecialidadRepository especialidadRepository) {
+        this.especialidadRepository = especialidadRepository;
+    }
+
+    @Override
+    public List<Especialidad> findAll() {
+        return especialidadRepository.findAll();
+    }
+
+    @Override
+    public Especialidad findById(Long id) {
+        return especialidadRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Especialidad save(Especialidad especialidad) {
+        return especialidadRepository.save(especialidad);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        especialidadRepository.deleteById(id);
+    }
+}
