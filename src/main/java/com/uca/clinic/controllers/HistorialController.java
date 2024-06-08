3e0acc4052930ce6c80376a2db39d6a9948b71e4
bleczz <00400519@uca.edu.sv>
2024-06-08 15:14:53 -0600

    partial implementation for /user/record/ GET method

--- src/main/java/com/uca/clinic/controllers/HistorialController.java
+++ src/main/java/com/uca/clinic/controllers/HistorialController.java
@@ -7,6 +7,7 @@ import com.uca.clinic.domain.entities.User;
 import com.uca.clinic.services.HistorialService;
 import com.uca.clinic.services.RolService;
 import com.uca.clinic.services.UserService;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,54 +18,84 @@ import org.springframework.web.bind.annotation.*;
 
 import com.uca.clinic.responses.GeneralResponse;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/historial")
+@RequestMapping("/api/user/record")
 public class HistorialController {
 
     private final UserService userService;
     private final RolService rolService;
     private final HistorialService historialService;
 
+    @Autowired
     public HistorialController(UserService userService, RolService rolService, HistorialService historialService) {
         this.userService = userService;
         this.rolService = rolService;
         this.historialService = historialService;
     }
 
+//    @GetMapping("/")
+//    public ResponseEntity<GeneralResponse> getAllHistorials(){
+//        List<Historial> historials = historialService.findAll();
+//        return GeneralResponse.getResponse(HttpStatus.OK, historials);
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<GeneralResponse> getHistorialById(@PathVariable Long id){
+//        Historial historial = historialService.findById(id);
+//        if (historial != null) {
+//            return GeneralResponse.getResponse(HttpStatus.OK, historial);
+//        } else {
+//            return GeneralResponse.getResponse(HttpStatus.NOT_FOUND, "Historial no encontrado");
+//        }
+//    }
+//
+//    @PreAuthorize("hasRole('DOCTOR')")
+//    @PostMapping("/")
+//    public ResponseEntity<GeneralResponse> createHistorial(@RequestBody @Valid HistorialDto historialDto, @AuthenticationPrincipal User userDetails){
+//        // get authenticated user
+//        User user = userService.findById(userDetails.getId());
+//
+//        if(user == null){
+//            return GeneralResponse.getResponse(HttpStatus.UNAUTHORIZED, "Usuario no authorizado");
+//        }
+//        Historial historial = historialService.save(user, historialDto);
+//        return GeneralResponse.getResponse(HttpStatus.CREATED, historial);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<GeneralResponse> deleteHistorial(@PathVariable Long id) {
+//        historialService.deleteById(id);
+//        return GeneralResponse.getResponse(HttpStatus.NO_CONTENT, "Historial eliminado");
+//    }
+
+
+    // CODE AFTER THIS POINT HAS BEEN CODED TAILORED TO THE EXAM REQUIREMENTS
+
+    //@RolesAllowed({"DOCTOR", "ASSISTANT"})
+   // @PostMapping("/")
+   // public ResponseEntity<GeneralResponse> createHistorial(@RequestBody @Valid HistorialDto historialDto, @AuthenticationPrincipal User userDetails){
+   //     // get authenticated user
+   //     User user = userService.findById(userDetails.getId());
+
+   //     if(user == null){
+   //         return GeneralResponse.getResponse(HttpStatus.NOT_FOUND, "Usuario no encontrado");
+   //     }
+   //     Historial historial = historialService.save(user, historialDto);
+   //     return GeneralResponse.getResponse(HttpStatus.CREATED, historial);
+   // }
+
+    //@RolesAllowed({"PATIENT"})
     @GetMapping("/")
-    public ResponseEntity<GeneralResponse> getAllHistorials(){
-        List<Historial> historials = historialService.findAll();
-        return GeneralResponse.getResponse(HttpStatus.OK, historials);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<GeneralResponse> getHistorialById(@PathVariable Long id){
-        Historial historial = historialService.findById(id);
-        if (historial != null) {
-            return GeneralResponse.getResponse(HttpStatus.OK, historial);
-        } else {
-            return GeneralResponse.getResponse(HttpStatus.NOT_FOUND, "Historial no encontrado");
-        }
-    }
-
-    @PreAuthorize("hasRole('DOCTOR')")
-    @PostMapping("/")
-    public ResponseEntity<GeneralResponse> createHistorial(@RequestBody @Valid HistorialDto historialDto, @AuthenticationPrincipal User userDetails){
-        // get authenticated user
+    public ResponseEntity<GeneralResponse> getHistorial(@AuthenticationPrincipal User userDetails, @RequestParam(required = false) Date startDate, @RequestParam(required = false) Date endDate){
         User user = userService.findById(userDetails.getId());
-
-        if(user == null){
-            return GeneralResponse.getResponse(HttpStatus.UNAUTHORIZED, "Usuario no authorizado");
+        List<Historial> historials = historialService.findUserHistorialWithinDateRange(user.getId(), startDate, endDate);
+        if (historials != null && !historials.isEmpty()) {
+            return GeneralResponse.getResponse(HttpStatus.OK, historials);
+        } else {
+            return GeneralResponse.getResponse(HttpStatus.NOT_FOUND, "No existen records disponibles en la fecha especificada");
         }
-        Historial historial = historialService.save(user, historialDto);
-        return GeneralResponse.getResponse(HttpStatus.CREATED, historial);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<GeneralResponse> deleteHistorial(@PathVariable Long id) {
-        historialService.deleteById(id);
-        return GeneralResponse.getResponse(HttpStatus.NO_CONTENT, "Historial eliminado");
     }
 }
--- src/main/java/com/uca/clinic/services/impl/HistorialServiceImpl.java
+++ src/main/java/com/uca/clinic/services/impl/HistorialServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,11 +31,11 @@ public class HistorialServiceImpl implements HistorialService {
         return historialRepository.findById(id).orElse(null);
     }
 
-    @Override
-    public Historial findByPatientId(Long patientId) {
-        Optional<Historial> historial = historialRepository.findByUserId(patientId);
-        return historial.orElse(null);
-    }
+    //@Override
+    //public Historial findByPatientId(Long patientId) {
+    //    Optional<Historial> historial = historialRepository.findByUserId(patientId);
+    //    return historial.orElse(null);
+    //}
 
     @Override
     public Historial save(User userDetails, HistorialDto historialDto) {
@@ -48,5 +49,18 @@ public class HistorialServiceImpl implements HistorialService {
     @Override
     public void deleteById(Long id) {
         historialRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Historial> findUserHistorialWithinDateRange(Long userId, Date startDate, Date endDate) {
+        if (startDate != null && endDate != null) {
+            return historialRepository.findByUserIdAndDateBetweenOrderByDateDesc(userId, startDate, endDate);
+        } else if (startDate != null) {
+            return historialRepository.findByUserIdAndDateAfterOrderByDateDesc(userId, startDate);
+        } else if (endDate != null) {
+            return historialRepository.findByUserIdAndDateBeforeOrderByDateDesc(userId, endDate);
+        } else {
+            return historialRepository.findByUserIdOrderByDateDesc(userId);
+        }
     }
 }
