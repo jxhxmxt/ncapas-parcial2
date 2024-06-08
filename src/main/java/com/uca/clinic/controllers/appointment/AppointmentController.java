3b39177c424be0fa3567c7cfe39fa6e471ac6e16
Steve9452 <00380119@uca.edu.sv>
2024-06-08 14:58:42 -0600

    fix conflict

--- src/main/java/com/uca/clinic/controllers/appointment/AppointmentController.java
+++ src/main/java/com/uca/clinic/controllers/appointment/AppointmentController.java
@@ -1,10 +1,17 @@
 package com.uca.clinic.controllers.appointment;
 
 
+import com.uca.clinic.domain.dtos.CitaMedicaDto;
+import com.uca.clinic.domain.entities.User;
 import com.uca.clinic.responses.GeneralResponse;
+import com.uca.clinic.services.CitaMedicaService;
+import com.uca.clinic.services.UserService;
 import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +20,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/appointment")
 public class AppointmentController {
 
+    private final UserService userService;
+
+    private final CitaMedicaService citaMedicaService;
+
+    @Autowired
+    public AppointmentController(UserService userService, CitaMedicaService citaMedicaService){
+        this.userService = userService;
+        this.citaMedicaService = citaMedicaService;
+    }
 
     @RolesAllowed({"ROLE_MEDICO", "ROLE_ADMIN"})
     @PermitAll
     @PostMapping("/request")
-    public ResponseEntity<GeneralResponse> requestAppointment(){
-        return ResponseEntity.ok(new GeneralResponse("Appointment requested", null));
+    public ResponseEntity<GeneralResponse> requestAppointment(@AuthenticationPrincipal User userDetails, CitaMedicaDto citaMedicaDto){
+        User _user = userService.findById(userDetails.getId());
+
+
+        return GeneralResponse.getResponse(HttpStatus.CREATED, "Success", citaMedicaService.save(_user, citaMedicaDto));
+    }
+
+
+    @RolesAllowed({"ROLE_ASISTENTE"})
+    @PostMapping("/request")
+    public ResponseEntity<GeneralResponse> approveAppointment(){
+
+
+        return GeneralResponse.getResponse("Appointment approved");
+
     }
 
 
